99a93f903c129a363c813e4b229d6e3e40986280
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-09-14 09:11:12 -0400

    v

--- src/components/featured/featured1.tsx
+++ src/components/featured/featured1.tsx
@@ -1,35 +1,35 @@
 export const Featured1 = () => {
-  return(
-    <section className="p-4 lg:p-8  text-gray-800">
-	<div className="container mx-auto space-y-12">
-		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-			<img src="https://source.unsplash.com/640x480/1" alt="" className="h-80 bg-gray-500 aspect-video" />
-			<div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
-				<span className="text-xs uppercase text-gray-600">Join, its free</span>
-				<h3 className="text-3xl font-bold">Were not reinventing the wheel</h3>
-				<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-				<button type="button" className="self-start">Action</button>
+	return (
+		<section className="p-4 lg:p-8  text-gray-800">
+			<div className="container mx-auto space-y-12">
+				<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
+					<img src="https://res.cloudinary.com/dqsbh2kn0/image/upload/v1655178759/samples/landscapes/architecture-signs.jpg" alt="" className="h-80 bg-gray-500 aspect-video" />
+					<div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
+						<span className="text-xs uppercase text-gray-600">Join, its free</span>
+						<h3 className="text-3xl font-bold">Were not reinventing the wheel</h3>
+						<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+						<button type="button" className="self-start">Action</button>
+					</div>
+				</div>
+				<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
+					<img src="https://res.cloudinary.com/dqsbh2kn0/image/upload/v1655178761/samples/landscapes/beach-boat.jpg" alt="" className="h-80 bg-gray-500 aspect-video" />
+					<div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
+						<span className="text-xs uppercase text-gray-600">Join, its free</span>
+						<h3 className="text-3xl font-bold">Were not reinventing the wheel</h3>
+						<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+						<button type="button" className="self-start">Action</button>
+					</div>
+				</div>
+				<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
+					<img src="https://res.cloudinary.com/dqsbh2kn0/image/upload/v1655178764/samples/landscapes/nature-mountains.jpg" alt="" className="h-80 bg-gray-500 aspect-video" />
+					<div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
+						<span className="text-xs uppercase text-gray-600">Join, its free</span>
+						<h3 className="text-3xl font-bold">Were not reinventing the wheel</h3>
+						<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+						<button type="button" className="self-start">Action</button>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-			<img src="https://source.unsplash.com/640x480/2" alt="" className="h-80 bg-gray-500 aspect-video" />
-			<div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
-				<span className="text-xs uppercase text-gray-600">Join, its free</span>
-				<h3 className="text-3xl font-bold">Were not reinventing the wheel</h3>
-				<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-				<button type="button" className="self-start">Action</button>
-			</div>
-		</div>
-		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-			<img src="https://source.unsplash.com/640x480/3" alt="" className="h-80 bg-gray-500 aspect-video" />
-			<div className="flex flex-col justify-center flex-1 p-6 bg-gray-50">
-				<span className="text-xs uppercase text-gray-600">Join, its free</span>
-				<h3 className="text-3xl font-bold">Were not reinventing the wheel</h3>
-				<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-				<button type="button" className="self-start">Action</button>
-			</div>
-		</div>
-	</div>
-</section>
-  )
+		</section>
+	)
 }
