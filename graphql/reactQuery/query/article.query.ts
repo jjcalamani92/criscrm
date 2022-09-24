90fb53203682c9d3aea0bee77fa9dbf2072dbbf8
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-09-24 01:33:26 -0400

    v

--- graphql/reactQuery/query/article.query.ts
+++ graphql/reactQuery/query/article.query.ts
@@ -1,14 +1,14 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { Blog, Page, Site, User } from "../../../interfaces";
+import { Article, Page, Site, User } from "../../../interfaces";
 
 import { FIND_ARTICLES } from "../../query/article.query";
 import { graphQLClient } from "../graphQLClient";
 
 export function useFindAllArticles() {
-  return useQuery<[Blog]>(["find-all-articles"], async () => {
-    const { getBlogs } = await graphQLClient.request(
+  return useQuery<[Article]>(["find-all-articles"], async () => {
+    const { findArticles } = await graphQLClient.request(
       FIND_ARTICLES,
     );
-    return getBlogs;
+    return findArticles;
   });
 }
