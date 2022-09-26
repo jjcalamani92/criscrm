03eb72755857acabb9dd04c3b3a4c172abbffb8b
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-09-26 17:50:30 -0400

    v

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -58,7 +58,7 @@ export const Routes: FC<Routes> = ({ }) => {
     case '/dashboard/sites': return <GridSite />
     case getPathsBySite(sitesPaths!, asPath): return <GridPage0 />
     case getPathsByPage0(sitesPaths!, asPath): return <GridPage1 />
-    // case getPathsByPage1(sitesPaths!, asPath): return <GridPage2 />
+    case getPathsByPage1(sitesPaths!, asPath): return <GridPage2 />
     // case getPathsByPage2(sitesPaths!, asPath): return <GridPage3 />
     // case getPathsByArticle(articles!, asPath): return <ArticleEdit/>
 
