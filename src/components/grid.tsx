61208d17469314238bdbebe73465c000fe06b11a
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-09-26 17:29:42 -0400

    v

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,46 +1,46 @@
 import { FC, useState } from 'react';
-import { useGetPage, useGetPages, useGetSites } from '../../graphql/reactQuery/reactQuery';
-import { Page, Site } from '../../interfaces/site.interface';
-import { CardSite } from './card';
-import { CardPage } from './card/cardPage';
-import { CardProduct } from './card/cardProduct';
-import { HeadingDashboard } from './heading';
-import { Modal } from './modal';
-import { Pagination } from './pagination';
-interface Grid {
-  sites?: Site[];
-  page?: Page | Site;
-  title: string;
-}
+// import { useGetPage, useGetPages, useGetSites } from '../../graphql/reactQuery/reactQuery';
+// import { Page, Site } from '../../interfaces/site.interface';
+// import { CardSite } from './card';
+// import { CardPage } from './card/cardPage';
+// import { CardProduct } from './card/cardProduct';
+// import { HeadingDashboard } from './heading';
+// import { Modal } from './modal';
+// import { Pagination } from './pagination';
+// interface Grid {
+//   sites?: Site[];
+//   page?: Page | Site;
+//   title: string;
+// }
 
-export const Grid: FC<Grid> = ({ sites, page, title }) => {
+// export const Grid: FC<Grid> = ({ sites, page, title }) => {
   
-  const { data: page2 } = useGetPage(page?._id!, page?.data.type!);
+//   const { data: page2 } = useGetPage(page?._id!, page?.data.type!);
 
-  // console.log(page?._id);
-  // console.log(page?.data.type);
-  // console.log(page2?.product);
+//   // console.log(page?._id);
+//   // console.log(page?.data.type);
+//   // console.log(page2?.product);
   
-  return (
-    <>
-      {sites && <HeadingDashboard title={'Sites'}  site={page as Site}/> }
-      {page && <HeadingDashboard title={title} uid={page._id} page={page as Page}/>}
+//   return (
+//     <>
+//       {sites && <HeadingDashboard title={'Sites'}  site={page as Site}/> }
+//       {page && <HeadingDashboard title={title} uid={page._id} page={page as Page}/>}
 
-      <div className={`grid grid-cols-2 gap-3 sm:gap-6  sm:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5`}>
+//       <div className={`grid grid-cols-2 gap-3 sm:gap-6  sm:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5`}>
 
-        {sites && sites.map((data, i) => <CardSite key={i} data={data} />)
-        }
+//         {sites && sites.map((data, i) => <CardSite key={i} data={data} />)
+//         }
         
-        {page && page.page.map((data, i) => <CardPage key={i} data={data} />)
-        }
+//         {page && page.page.map((data, i) => <CardPage key={i} data={data} />)
+//         }
 
         
-        {
-          // page?.data.type === 'clothing' && page2?.product.map((data, i) => <CardProduct key={i} data={data} />)
-        }
+//         {
+//           // page?.data.type === 'clothing' && page2?.product.map((data, i) => <CardProduct key={i} data={data} />)
+//         }
         
-      </div>
-      <Pagination />
-    </>
-  )
-}
+//       </div>
+//       <Pagination />
+//     </>
+//   )
+// }
