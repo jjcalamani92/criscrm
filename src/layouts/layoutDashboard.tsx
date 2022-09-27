87916f248c664753da251e09209025ae924cacca
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-09-26 20:37:15 -0400

    v

--- src/layouts/layoutDashboard.tsx
+++ src/layouts/layoutDashboard.tsx
@@ -16,7 +16,7 @@ export const LayoutDashboard: FC<LayoutDashboard> = ({ children }) => {
   const { query, asPath } = useRouter()
   const { data: sitesSeo } = useSitesSeo();
   const seo = getProBySites(sitesSeo!).find(data => data.path === asPath)?.seo
-  console.log(seo);
+  // console.log(seo);
   
   return (
     <>
@@ -27,9 +27,9 @@ export const LayoutDashboard: FC<LayoutDashboard> = ({ children }) => {
           <meta name="keywords" />
           <meta name="description" content={seo ? seo?.description : 'description'} />
           <meta property="og:title" content={seo ? seo?.title : 'criscrm'} />
-          <meta property="og:description" content={seo ? seo.description : 'description'} />
+          <meta property="og:description" content={seo ? seo?.description : 'description'} />
           <meta property="og:type" content="og:product" />
-          {seo && seo.image && <meta property="og:image" content={seo ? seo?.image.src : "https://res.cloudinary.com/dqsbh2kn0/image/upload/v1662447369/qgy7hht1b12tfc8tyrx8.jpg"} />}
+          {seo && seo?.image && <meta property="og:image" content={seo ? seo?.image.src : "https://res.cloudinary.com/dqsbh2kn0/image/upload/v1662447369/qgy7hht1b12tfc8tyrx8.jpg"} />}
           <link rel="icon" href={"https://res.cloudinary.com/dqsbh2kn0/image/upload/v1662447369/qgy7hht1b12tfc8tyrx8.jpg"} />
         </Head>
         <HeaderDashboard />
